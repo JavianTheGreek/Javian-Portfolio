d6bb2b31d0b2a85ed87ecb4d06a0943392b3cf0b
Javian <javianbedward0917@gmail.com>
2021-05-22 17:10:18 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 console.log('The website still not finish, but its okay thought..... ');
+console.warn('This is a warning!!!');
+console.error('I think you made a error???');
+
+console.group()
+  console.log('');
+  console.group('');
+    console.log('');
+    console.log('');
+  console.groupEnd('');
+console.groupEnd()
